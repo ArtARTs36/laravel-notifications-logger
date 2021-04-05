0ea7e10e83a377efcf44e64a493aba10866d176e
ArtARTs36 <temicska99@mail.ru>
2021-04-05 04:53:41 +0300

    systems index route

---
+++ src/Http/Controllers/SystemController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace ArtARTs36\LaravelNotificationsLogger\Http\Controllers;
+
+use ArtARTs36\LaravelNotificationsLogger\Models\System;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Routing\Controller;
+
+class SystemController extends Controller
+{
+    public function index(): AnonymousResourceCollection
+    {
+        return JsonResource::collection(System::all());
+    }
+}
